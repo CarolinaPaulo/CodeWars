1f0fb8aa9e1b42e3408aee245ef8ebc471e6e41d
Carolina de Faria Paulo <63932855+CarolinaPaulo@users.noreply.github.com>
2021-03-12 10:29:19 -0300

    new_file

---
+++ Java/(7 kyu)_Easy_wallpaper.java
@@ -0,0 +1,53 @@
+package Java;/* John wants to decorate a room with wallpaper. He wants a fool-proof method for getting it right.
+
+John knows that the rectangular room has a length of l meters, a width of w meters, a height of h meters. The standard width of the rolls he wants to buy is 52 centimeters. The length of a roll is 10 meters. He bears in mind however, that it’s best to have an extra length of wallpaper handy in case of mistakes or miscalculations so he wants to buy a length 15% greater than the one he needs.
+
+Last time he did these calculations he got a headache, so could you help John?
+
+Task
+Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
+
+Example:
+wallpaper(4.0, 3.5, 3.0) should return "ten"
+
+wallpaper(0.0, 3.5, 3.0) should return "zero"
+
+Notes:
+all rolls (even with incomplete width) are put edge to edge
+0 <= l, w, h (floating numbers); it can happens that w * h * l is zero
+the integer r (number of rolls) will always be less or equal to 20
+FORTH: the number of rolls will be a positive or null integer (not a plain English word; this number can be greater than 20)
+
+In Javascript English numbers are preloaded and can be accessed as:
+
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+For other languages it is not preloaded but you can use the above one if you need it. */
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class EasyWallpaper {
+
+  public String wallpaper(double l, double w, double h) {
+    String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+    List<String>  list = new ArrayList<>();
+    list.addAll(Arrays.asList(numbers));
+    if(l<=0 || w<=0 || h <=0){
+      return "zero";
+    }
+
+    else{
+      double sizeWall = ((l*h*2) + (h*w*2));
+      double rolls = 1.15*(sizeWall/5.2);
+      int sizePaper= (int) Math.ceil(rolls);
+
+      String r = list.get(sizePaper);
+
+
+    return r;
+  }
+}
+}
